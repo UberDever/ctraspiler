60de4989fac485df8cfa885ba1c8e37876f91cdc
uber <uberdever@mail.ru>
2024-12-22 17:40:38 +0300

    Copied parsing routines to main

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
-import "flag"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	p "some/ast"
+	s "some/syntax"
+	u "some/util"
+	"strings"
+
+	"golang.org/x/exp/utf8string"
+)
 
 func main() {
 	filepath := flag.String("path", "undefined", "filepath to source")
 	flag.Parse()
-	_ = filepath
+	contents, err := ioutil.ReadFile(*filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	text := utf8string.NewString(string(contents))
+	src := s.NewSource("ast_test", *text)
+
+	handler := u.NewHandler()
+	tokenizer := s.NewTokenizer(&handler)
+	tokenizer.Tokenize(&src)
+	if !handler.IsEmpty() {
+		errs := handler.AllErrors()
+		panic(errors.New(strings.Join(errs, "")))
+	}
+
+	parser := p.NewParser(&handler)
+	ast := parser.Parse(&src)
+	if !handler.IsEmpty() {
+		errs := handler.AllErrors()
+		panic(errors.New(strings.Join(errs, "")))
+	}
+
+	dump := ast.Dump(0)
+	formatted := u.FormatSExpr(dump)
+
+	fmt.Println(formatted)
 }
